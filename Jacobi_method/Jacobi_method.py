45b8475bbbb05986b4083f515b376eade6bd8c7e
puneeth dambe <puneethdambe@outlook.com>
2024-05-09 23:23:19 +0530

    The jacobi method works fine, need to adjuct threshold error and threshold iteration as per the requirement. The threshold error increases with size of matrix

--- Jacobi_method/Jacobi_method.py
+++ Jacobi_method/Jacobi_method.py
@@ -15,54 +15,60 @@ import numpy as np
 N= 100 # size of vector
 
 b= np.ones(N)
+
+b_exact= np.ones(N-2)
 #b[10-1]=10
 # Consider the Matrix A = [ 2 -1 0......0
 #                           -1 2 -1 0.....0
 #                           0 -1 2 -1 0...0] (N)X(N)
-A = np.array(np.zeros((N,N)))
+A = np.array(np.zeros((N-2,N-2)))
 
-for i in range(0,N):
+for i in range(0,np.shape(A)[0]):
     A[i,i]=2
     
-for i in range(0,N-1): 
+for i in range(0,np.shape(A)[0]-1): 
     A[i,i+1]=-1
 
-for i in range(1,N): 
+for i in range(1,np.shape(A)[0]): 
     A[i,i-1]=-1
    
 # Now solving through Jacobi method
 
-threshold_error=0.01
-threshold_iteration= 20
+threshold_error=0.00001 
+threshold_iteration= 10000 # Maximum number of iteration to be run 
+                            # irrespective of threshold error
 x_prev_iter= np.zeros_like(b)
-x_new_iter= np.zeros_like(b)
-sum_error= 0
 
 k=0
 while True:
-    error=np.zeros_like(b)
+    
+    x_new_iter= np.zeros_like(b)
+    error= np.zeros_like(b)
     x_new_iter[1:-1]= (b[1:-1]+ x_prev_iter[0:-2]+x_prev_iter[2:])/2
-    #error= x_new_iter[0:]-x_prev_iter[0:]
+        
+    sum_error= 0
+    for i in range(0,np.shape(error)[0]):
+       error[i]= x_new_iter[i]-x_prev_iter[i]
+       sum_error= sum_error+error[i]**2   
+    rms_error= np.sqrt(sum_error/(np.shape(error)[0]-2))
     
-    #for i in range(0,np.shape(error)[0]):
-     #   sum_error= sum_error+error[i]**2   
-    
-    #rms_error= np.sqrt(sum_error/(np.shape(error)[0]-2))
-    #print(x_prev_iter)
-    print(x_new_iter)
     #print(error)
     #print(sum_error)
-    #print(rms_error)
-    print(k)
-    x_prev_iter= x_new_iter
+    print("The rms error is ",rms_error)
+    print("The number of iteration ",k)
+    
     k=k+1
-    #if rms_error< threshold_error:
-    #    break
-    if k> threshold_iteration:
-        break
+    if k>threshold_iteration:
+       break
+    if rms_error< threshold_error:
+       break 
     
+    x_prev_iter= x_new_iter
+    
+# Using Inverse matrix ( Direct method)
 A_inv= np.linalg.inv(A)
-x_exact= np.zeros_like(b)
-x_exact= np.dot(A_inv,b)
-#print(x_exact)    
-    
+x_exact= np.zeros(N-2)
+x_exact= np.dot(A_inv,b_exact)
+
+print("Solution using Direct Method ", x_exact)    
+print("Solution using Iterative Jacobi Method",x_new_iter)    
