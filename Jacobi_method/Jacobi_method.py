72f7de89ed532168c754c37f0cbf3f3c7c11c794
puneeth dambe <puneethdambe@outlook.com>
2024-05-09 17:33:38 +0530

    Added RHS vector

--- Jacobi_method/Jacobi_method.py
+++ Jacobi_method/Jacobi_method.py
@@ -1,1 +1,17 @@
+# Code to solve linear system of equation using Jacobi Method
 
+# Author: PUNEETHA KUMAR D
+# MTech in Mechanical Engineering IISc Bangalore
+# Date: 09/05/2024
+
+# Consider a linear system of equation in the form Ax= b
+
+# Let Rhs be unit vector
+
+from tkinter import N
+import numpy as np
+
+
+N= 100 # size of vector
+
+b= np.ones(N)
